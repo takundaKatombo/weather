56d00b985597f53f948c64cb7a1994004319bb41
takundaKatombo <mtkatombo@gmail.com>
2023-11-27 17:00:58 +0200

    minor

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,13 +1,8 @@
-import 'dart:convert';
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:weather/colors.dart';
 import 'package:weather/models/forecast_weather_model/forecast_weather_model.dart';
-import 'package:weather/private.dart';
-import 'package:weather/services/api_calls.dart';
-import 'package:http/http.dart' as http;
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({super.key, required this.forecast});
@@ -37,14 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // Center(
-            //   child: const Text(
-            //     'You Dont have Saved Locations',
-            //     style: TextStyle(fontSize: 30, fontFamily: 'Poppins'),
-            //   ),
-            // ),
             //TODO: add a button to go to search page
             SizedBox(
               height: MediaQuery.sizeOf(context).height * 0.1,
@@ -52,7 +40,6 @@ class _MyHomePageState extends State<MyHomePage> {
             Container(
               padding: EdgeInsets.only(left: 20, right: 20),
               width: MediaQuery.sizeOf(context).width,
-              // height: MediaQuery.sizeOf(context).height * 0.01,
               child: Row(
                 children: [
                   Icon(
@@ -348,47 +335,6 @@ class _MyHomePageState extends State<MyHomePage> {
                         )
                       ],
                     ),
-                    // Column(
-                    //   children: [
-                    //     Text(
-                    //       snapshot.data!.forecast!.forecastday![3]
-                    //               .date ??
-                    //           "",
-                    //       style: TextStyle(
-                    //         fontFamily: 'Roboto',
-                    //         fontSize: 14, fontWeight: FontWeight.w700,
-                    //         // fontFeatures: [FontFeature.superscripts()],
-                    //         color: Colors.white,
-                    //         // height: 16,
-                    //       ),
-                    //     ),
-                    //     Image.network(
-                    //       "https:${snapshot.data!.forecast!.forecastday![3].day!.condition!.icon}",
-                    //       width: 50,
-                    //       height: 50,
-                    //       fit: BoxFit.fill,
-                    //     ),
-                    //     Text(
-                    //       "${snapshot.data!.forecast!.forecastday![3].day!.avgtempC}°C",
-                    //       style: TextStyle(
-                    //         fontFamily: 'Roboto',
-                    //         fontSize: 14, fontWeight: FontWeight.w700,
-                    //         // fontFeatures: [FontFeature.superscripts()],
-                    //         color: Colors.white,
-                    //         // height: 16,
-                    //       ),
-                    //     ),
-                    //     Text(
-                    //       "${snapshot.data!.forecast!.forecastday![3].day!.maxwindKph}\nkm/h",
-                    //       style: TextStyle(
-                    //         fontFamily: 'Roboto',
-                    //         fontSize: 14, fontWeight: FontWeight.w500,
-                    //         color: Colors.white,
-                    //         // height: 16,
-                    //       ),
-                    //     )
-                    //   ],
-                    // )
                   ],
                 ),
               ),
